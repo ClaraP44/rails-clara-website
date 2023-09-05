f379d216dae9614e411d78fd4aa74ab9e749ada7
clara pecot <clara.pecot44@gmail.com>
2023-09-05 14:31:30 +0200

    NAVBAR : link changes color when current page

---
+++ app/assets/javascripts/navigation.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var currentPath = window.location.pathname;
+  var navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach(function (link) {
+    var linkPath = link.getAttribute("href");
+    if (currentPath.startsWith(linkPath)) {
+      link.classList.add("active");
+    }
+  });
+});
